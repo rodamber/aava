354ae26eeee0c55fbd7d5f8839cec02dd3d19322
rodamber <rodrigoambernardo@gmail.com>
2017-04-29 16:20:32 +0100

    Implemented bad char and strong good suffix rules.

--- src/Haskell.hs
+++ src/Haskell.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE Strict #-}
 module Haskell where
 
+import Data.Foldable
+import qualified Data.Map as M
 import Data.Maybe
+import qualified Data.Sequence as S
 import qualified Data.Text as T
 
 --------------------------------------------------------------------------------
@@ -46,5 +49,57 @@ zAlgorithmSpec :: T.Text -> [Int]
 zAlgorithmSpec txt = 0 : (zMatchCount txt 1 <$> [2 .. T.length txt])
 
 reverseZAlgorithmSpec = zAlgorithmSpec . T.reverse
+
+--------------------------------------------------------------------------------
+
+badCharPreprocessingSpec :: T.Text -> M.Map Char Int
+badCharPreprocessingSpec t =
+  (flip foldMap) ['A','C','T','G'] $ \c ->
+    case T.findIndex (== c) (T.reverse t) of
+      Just ix -> M.singleton c (len - 1 - ix)
+      Nothing -> M.empty
+  where len = T.length t
+
+badCharShiftSpec :: M.Map Char Int -> Int -> Char -> Int
+badCharShiftSpec r ix c = max 1 (ix - x)
+  where x = case M.lookup c r of
+              Just a -> a
+              Nothing -> error "bad char shift: impossible char"
+
+--------------------------------------------------------------------------------
+
+buildBigL' :: T.Text -> S.Seq Int
+buildBigL' t = S.fromList $
+  (flip fmap) [1 .. n] $ \i -> maximum $
+    (flip fmap) [i .. n - 1] $ \j ->
+      if bigN `S.index` j == n - i + 1 then j else 0
+  where
+    n = T.length t
+    bigN = S.fromList (reverseZAlgorithmSpec t)
+
+buildL' :: T.Text -> S.Seq Int
+buildL' t = S.fromList $
+  (flip fmap) [1 .. n] $ \i -> maximum $
+    (flip fmap) [1 .. n - i + 1] $ \j ->
+      if bigN `S.index` j == j then j else 0
+  where
+    n = T.length t
+    bigN = S.fromList (reverseZAlgorithmSpec t)
+
+strongGoodSuffixPreprocessingSpec :: T.Text -> (S.Seq Int, S.Seq Int)
+strongGoodSuffixPreprocessingSpec t = (buildBigL' t, buildL' t)
+
+data Match = Match | Mismatch Int
+
+strongGoodSuffixShiftSpec :: (S.Seq Int, S.Seq Int) -> Match -> Int
+strongGoodSuffixShiftSpec (_, l') Match =
+  S.length l' - l' `S.index` 2
+strongGoodSuffixShiftSpec (bigL', l') (Mismatch j)
+  | bigL'i >  0 = n - bigL'i
+  | bigL'i == 0 = n - l' `S.index` i
+  | i == 1      = 1
+  where bigL'i = bigL' `S.index` i
+        n = S.length l'
+        i = j + 1
 
 --------------------------------------------------------------------------------
