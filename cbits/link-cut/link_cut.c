6199bc53b8ac95fe3b03e17f3ac6890d942df6c4
rodamber <rodrigoambernardo@gmail.com>
2017-05-06 22:53:32 +0100

    Implemented link/cut ops. as per the paper.

--- cbits/link-cut/link_cut.c
+++ cbits/link-cut/link_cut.c
@@ -17,66 +17,6 @@ bool not(bool b) { return !b; }
 /* Link/Cut tree internal representation                                       */
 /*----------------------------------------------------------------------------*/
 
-/*
-   Operations that do not change the forest
-   ----------------------------------------
-
-   - parent(vertex v): return the parent of v. if v has no parent (it is a tree root), return null
-   - root(vertex v): return the root of the tree containing v
-
-   Operations that change the forest
-   ---------------------------------
-
-   - link(vetex v, w): combine the trees containing v and w by adding the edge
-                       (v,w) of cost x, making w the parent of v. THIS OPERATION ASSUMES THAT V IS
-                       NOT A TREE ROOT.
-   - cut(vertex v): divide the tree containing vertex v into two trees by
-                    deleting the edge(v,parent(v)); THIS OPERATION ASSUMES THAT V
-                    IS NOT A TREE ROOT.
-   - evert(vertex v): Modify the tree containing vertex v by making v the root.
-                     (This operation can be regarded as reversing the direction
-                     of everty edge on the path from v to the original root.)
-
-   Nodes storage
-   -------------
-
-   - dparent(v): ("dashed parent") the parent of v (via an outgoing dashed edge)
-                 IF IT IS THE TAIL OF THE PATH.
-
-   Primitive operations
-   --------------------
-
-   - path(vertex v): return the path containing v
-   - head(path p): return the bottommost vertex of the path
-   - tail(path p): return the topmost vertex of the path
-   - before(vertex v): return the vertex before v on path(v), or null if v is
-                       the head of the path
-   - after(vertex v): return vertex after v on path(v), or null if v is the tail
-                      of the path
-   - reverse(path p): reverse the direction of p, making the head the tail and
-                      vice versa
-   - concatenate(path p, path q): combine p and q by adding the edge (tail(p),
-                                  head(q)) and return the combined path
-   - split(vertex v): divide path(v) into up to 3 parts: the vertices from
-                      head(path(v)) to before(v) (p), v, and those from after(v) to
-                      tail(path(v)) (q). (v = head(path(v)) => p = null &&
-                                          v = tail(path(v)) => q = null);
-
-   Composite operations
-   --------------------
-
-   - splice(path p): Extend the solid path p by converting the dashed edge
-                     leaving tail(p) to solid and converting the original solid
-                     edge entering parent(tail(p)) (if any) to dashed. Return
-                     the extended path. THIS OPERATION ASSUMES THAT tail(p) IS
-                     NOT A TREE ROOT (THAT IS, THERE IS A DASHED EDGE LEAVING
-                     tail(p));
-   - expose(vertex v): Create a single solid path with head v and tail root(v)
-                     by converting dashed edges to solid along the tree path
-                     from v to root(v) and converting solid edges incident to
-                     this path to dashed. Return the resulting solid path.
- */
-
 struct node {
   struct node *left;
   struct node *right;
@@ -89,8 +29,11 @@ typedef node path;
 
 node *left(node *x) { return undefined("left", x); }
 node *right(node *x) { return undefined("right", x); }
-node *parent(node *x) { return undefined("parent", x); }
-node *dparent(node *x) { return undefined("dparent", x); }
+
+/* dparent(v): ("dashed parent") the parent of v (via an outgoing dashed edge)
+               IF IT IS THE TAIL OF THE PATH. */
+node **dparent(node *x) { return undefined("dparent", x); }
+
 
 /*----------------------------------------------------------------------------*/
 /* Primitive operations                                                       */
@@ -132,10 +75,10 @@ void split(path **p, node *x, path **q) { undefined("split", p, x, q); }
    on entry dparent(tail(p)) /= null." */
 void splice(path *p) {
   path *q, *r;
-  node *x = dparent(tail(p));
+  node *x = *dparent(tail(p));
 
   split(&q, x, &r);
-  if (q) *dparent(tail(q)) = *x;
+  if (q) *dparent(tail(q)) = x;
   concatenate(p, path_of(x));
 }
 
@@ -143,10 +86,60 @@ void expose(node *x) {
   path *q, *r;
   split(&q, x, &r);
 
-  if (q) *dparent(tail(q)) = *x;
+  if (q) *dparent(tail(q)) = x;
   if (r) concatenate(path_of(x), r);
   for (; dparent(tail(path_of(x))); splice(x));
 }
+
+/*----------------------------------------------------------------------------*/
+/* Operations that do not change the forest                                   */
+/*----------------------------------------------------------------------------*/
+
+/* parent(vertex v): return the parent of v. if v has no parent (it is a tree
+   root), return null */
+node *parent(node *x) {
+  if (x == tail(path_of(x)))
+    return *dparent(x);
+  return after(x);
+}
+
+/* root(vertex v): return the root of the tree containing v */
+node *root(node *x) {
+  expose(x);
+  return tail(path_of(x));
+}
+
+/*----------------------------------------------------------------------------*/
+/* Operations that change the forest                                          */
+/*----------------------------------------------------------------------------*/
+
+/* link(vertex v, w): combine the trees containing v and w by adding the edge
+                      (v,w) of cost x, making w the parent of v. THIS OPERATION
+                      ASSUMES THAT V IS NOT A TREE ROOT. */
+void link(node *x, node *y) {
+  expose(y);
+  concatenate(path_of(x), path_of(y));
+}
+
+/* cut(vertex v): divide the tree containing vertex v into two trees by deleting
+                  the edge(v,parent(v)); THIS OPERATION ASSUMES THAT V IS NOT A
+                  TREE ROOT. */
+void cut(node *x) {
+  path *p, *q;
+  expose(x);
+  split(&p, x, &q);
+  *dparent(x) = NULL;
+}
+
+/* evert(vertex v): Modify the tree containing vertex v by making v the root.
+                    (This operation can be regarded as reversing the direction
+                    of everty edge on the path from v to the original root.) */
+void evert(node *x) {
+  expose(x);
+  reverse(path_of(x));
+  *dparent(x) = NULL;
+}
+
 
 /*----------------------------------------------------------------------------*/
 /* Project operations                                                         */
