fc5b6c6c20cc13c5db9517d21038af77b77e853b
rodamber <rodrigoambernardo@gmail.com>
2017-05-19 00:48:49 +0100

    unfortunately couldn't use haskell (probably because i'm a noob)

--- app/online-matching/profile/Main.hs
+++ app/online-matching/profile/Main.hs
@@ -3,62 +3,37 @@
 module Main where
 
 import Control.Monad
+
+import qualified Data.ByteString.Char8 as C
 import Data.Traversable
+import Data.Vector
+
 import System.Environment
+import System.IO
 import System.Process
-import Test.QuickCheck
+import System.Random
+
 import Text.Printf
 import Text.Read
 
---------------------------------------------------------------------------------
+chars :: Vector Char
+chars = fromList ['A','C','T','G']
 
-data Algorithm = N | K | B deriving (Eq, Show)
-
-run = do
-  forM (map (2^) [5..30]) $ \txtSize -> do
-    forM (map (2^) [3..7]) $ \patSize -> do
-      when (txtSize >= patSize) $ do
-        txt <- generate (textGen txtSize)
-        pat <- generate (textGen patSize)
-
-        (naiveTime, naiveMem, naiveCmps) <- profile N txt pat
-        (kmpTime,   kmpMem,   kmpCmps)   <- profile K txt pat
-        (bmTime,    bmMem,    bmCmps)    <- profile B txt pat
-
-        let format = unwords (replicate 10 "%d") ++ "\n"
-        printf format txtSize    patSize
-                      naiveTime  naiveMem
-                      kmpTime    kmpMem    kmpCmps
-                      bmTime     bmMem     bmCmps
-
-textGen :: Int -> Gen String
-textGen n = resize n $ listOf1 $ elements ['A', 'C', 'T', 'G']
-
-profile :: Algorithm -> String -> String -> IO (Int,Int,Int)
-profile search txt pat = return (0,0,0)
-
-time :: Algorithm -> String -> String -> IO (Int,Int)
-time alg txt pat = do
-  let cmd = "time.sh"
-  let args = ["./cbits/online-matching/om"]
-
-  (elapsed:rest) <- lines <$> readProcess cmd args (input alg txt pat)
-
-  return $ case alg of
-             N -> (read elapsed, 0)
-             K -> (read elapsed, read $ head rest)
-             B -> (read elapsed, read $ head rest)
-
-
+main :: IO ()
 main = do
-  time N "AAAAAAAAT" "AAT"
-  -- print =<< time N "AAAAAAAAT" "AAT"
-  -- print =<< time K "AAAAAAAAT" "AAT"
-  -- print =<< time B "AAAAAAAAT" "AAT"
+  forM_ (map (2^) [5..20]) $ \txtSize -> do
+    forM_ (map (2^) [3..10]) $ \patSize -> do
 
-input :: Algorithm -> String -> String -> String
-input alg txt pat = unlines ["T " ++ txt, show alg ++ " " ++ pat, "X"]
+      when (txtSize >= patSize) $ do
+        let fileName = _ -- makeFileName txtSize patSize
+        write fileName txtSize patSize
 
+write :: String -> Int -> Int -> IO ()
+write = undefined
 
+        -- ix <- randomIO
+        -- let ch = chars ! (ix `mod` 4)
 
---------------------------------------------------------------------------------
+        -- putStr "T "
+        -- putChar ch
+
